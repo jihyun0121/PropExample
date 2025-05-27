201f1daaffb93b11d9863d97f310c02c836d6119
gheun <gheun@DESKTOP-ARR6LC5>
2025-05-27 14:51:43 +0900

    고전적인 스타일로 Box 정의

---
+++ Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    internal class Box
+    {
+        private int width;
+        private int height;
+
+        public Box(int width, int height)
+        {
+            if (width < 0 || height < 0)
+            {
+                this.width = width;
+                this.height = height;
+            }
+            else
+            {
+                Console.WriteLine("너비와 높이는 자연수로 초기화 해주세요");
+            }
+        }
+
+        public int getWidth() { return this.width; }
+        public int getHeight() { return this.height; }
+
+        public int Area()
+        {
+            return this.width * this.height;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(100, 200);
+            Console.WriteLine(box1.Area());
+        }
+    }
+}
